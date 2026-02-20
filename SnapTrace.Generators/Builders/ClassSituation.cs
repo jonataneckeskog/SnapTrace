728119d4416925a734c7bef01f43068b0aee2278
jonataneckeskog <jonataneckeskog@gmail.com>
2026-02-20 23:49:45 +0100

    Feat: Add ClassSituation with comments

---
+++ SnapTrace.Generators/Builders/ClassSituation.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SnapTrace.Generators.Builders;
+
+[Flags]
+public enum ClassSituation
+{
+    None = 0,
+    Static = 1 << 0,      // Class is static (No 'this' parameter at all)
+    Unsafe = 1 << 1,      // Class relies on unsafe context
+    IsStruct = 1 << 2,    // Requires 'this ref' in the interceptor signature
+    IsRefStruct = 1 << 3, // Requires 'this ref' AND prevents async interceptors
+    IsGeneric = 1 << 4    // Class has <T>, so interceptor needs those type arguments
+}
