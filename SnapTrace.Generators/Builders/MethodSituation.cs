45e95696a52762a9ef49beb363f2fa3e4f1b45a1
jonataneckeskog <jonataneckeskog@gmail.com>
2026-02-20 23:45:34 +0100

    Feat: Add MethodSituation

---
+++ SnapTrace.Generators/Builders/MethodSituation.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SnapTrace.Generators.Builders;
+
+[Flags]
+public enum MethodSituation
+{
+    None = 0,
+    Static = 1 << 0,
+    Async = 1 << 1,
+    Unsafe = 1 << 2,
+    Generic = 1 << 3,
+    ReturnsRef = 1 << 4,
+    ReturnsRefReadonly = 1 << 5
+}
